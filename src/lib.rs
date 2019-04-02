3d1e84ac26b52b1269272ef9b4e6b80c6e7fc4f0
Matthieu Pizenberg <matthieu.pizenberg@gmail.com>
2019-04-02 19:08:34 +0200

    Implement game of life with canvas

--- src/lib.rs
+++ src/lib.rs
@@ -56,6 +56,18 @@ impl Universe {
 /// Public methods, exported to JavaScript.
 #[wasm_bindgen]
 impl Universe {
+    pub fn width(&self) -> u32 {
+        self.width
+    }
+
+    pub fn height(&self) -> u32 {
+        self.height
+    }
+
+    pub fn cells(&self) -> *const Cell {
+        self.cells.as_ptr()
+    }
+
     pub fn tick(&mut self) {
         let mut next = self.cells.clone();
 
@@ -89,8 +101,8 @@ impl Universe {
     }
 
     pub fn new() -> Universe {
-        let width = 32;
-        let height = 32;
+        let width = 64;
+        let height = 64;
 
         let cells = (0..width * height)
             .map(|i| {
