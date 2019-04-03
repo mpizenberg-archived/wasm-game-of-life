7cfbb11a8d22ff33c7e018be09e9a2c7334b2a42
Matthieu Pizenberg <matthieu.pizenberg@gmail.com>
2019-04-03 15:34:30 +0200

    Optimized game of life with canvas

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 mod utils;
 
 use cfg_if::cfg_if;
-use std::fmt;
+use std::mem;
 use wasm_bindgen::prelude::*;
 
 cfg_if::cfg_if! {
@@ -13,49 +13,86 @@ cfg_if::cfg_if! {
     }
 }
 
-#[wasm_bindgen]
-#[repr(u8)]
-#[derive(Clone, Copy, Debug, PartialEq, Eq)]
-pub enum Cell {
-    Dead = 0,
-    Alive = 1,
+#[derive(Clone, Copy)]
+enum Cell {
+    Dead,
+    Alive,
 }
 
 #[wasm_bindgen]
 pub struct Universe {
     width: u32,
     height: u32,
+    canvas: Vec<u8>,
     cells: Vec<Cell>,
+    cells_tmp: Vec<Cell>,
 }
 
 /// Private methods not exposed to JavaScript.
 impl Universe {
-    // #[inline]
-    fn get_index(&self, row: u32, col: u32) -> usize {
+    #[inline]
+    fn cell_index(&self, row: u32, col: u32) -> usize {
         (row * self.width + col) as usize
     }
 
+    #[inline]
     fn live_neighbor_count(&self, row: u32, col: u32) -> u8 {
         let mut count = 0;
-        for &delta_col in [self.width - 1, 0, 1].iter() {
-            for &delta_row in [self.height - 1, 0, 1].iter() {
+        for &delta_row in [self.height - 1, 0, 1].iter() {
+            for &delta_col in [self.width - 1, 0, 1].iter() {
                 // Skip center point.
                 if delta_row == 0 && delta_col == 0 {
                     continue;
                 }
-                let neighbor_col = (col + delta_col) % self.width;
                 let neighbor_row = (row + delta_row) % self.height;
-                let index = self.get_index(neighbor_row, neighbor_col);
-                count += self.cells[index] as u8;
+                let neighbor_col = (col + delta_col) % self.width;
+                let index = self.cell_index(neighbor_row, neighbor_col);
+                // if let Cell::Alive = self.cells[index] {
+                if let Cell::Alive = unsafe { self.cells.get_unchecked(index) } {
+                    count += 1;
+                }
             }
         }
         count
+    }
+
+    // Canvas methods.
+
+    #[inline]
+    fn make_dead(&mut self, index: usize) {
+        *unsafe { self.canvas.get_unchecked_mut(index) } = 0;
+        *unsafe { self.canvas.get_unchecked_mut(index + 1) } = 0;
+        *unsafe { self.canvas.get_unchecked_mut(index + 2) } = 0;
+        *unsafe { self.canvas.get_unchecked_mut(index + 3) } = 255;
+    }
+
+    #[inline]
+    fn make_alive(&mut self, index: usize) {
+        *unsafe { self.canvas.get_unchecked_mut(index) } = 255;
+        *unsafe { self.canvas.get_unchecked_mut(index + 1) } = 255;
+        *unsafe { self.canvas.get_unchecked_mut(index + 2) } = 255;
+        *unsafe { self.canvas.get_unchecked_mut(index + 3) } = 255;
+    }
+
+    fn update_canvas_from_cells(&mut self) {
+        let size = self.cells.len();
+        for index in 0..size {
+            let canvas_index = 4 * index;
+            match self.cells[index] {
+                Cell::Alive => self.make_alive(canvas_index),
+                Cell::Dead => self.make_dead(canvas_index),
+            }
+        }
     }
 }
 
 /// Public methods, exported to JavaScript.
 #[wasm_bindgen]
 impl Universe {
+    pub fn canvas_data(&self) -> *const u8 {
+        self.canvas.as_ptr()
+    }
+
     pub fn width(&self) -> u32 {
         self.width
     }
@@ -64,47 +101,35 @@ impl Universe {
         self.height
     }
 
-    pub fn cells(&self) -> *const Cell {
-        self.cells.as_ptr()
-    }
-
     pub fn tick(&mut self) {
-        let mut next = self.cells.clone();
-
-        for col in 0..self.width {
-            for row in 0..self.height {
-                let idx = self.get_index(row, col);
-                let cell = self.cells[idx];
+        // Update cells.
+        for row in 0..self.height {
+            for col in 0..self.width {
+                let idx = self.cell_index(row, col);
+                let cell = unsafe { self.cells.get_unchecked(idx) };
+                // let cell = &self.cells[idx];
                 let live_neighbors = self.live_neighbor_count(row, col);
-
                 let next_cell = match (cell, live_neighbors) {
-                    // Rule 1: Any live cell with fewer than two live neighbours
-                    // dies, as if caused by underpopulation.
-                    (Cell::Alive, x) if x < 2 => Cell::Dead,
-                    // Rule 2: Any live cell with two or three live neighbours
-                    // lives on to the next generation.
                     (Cell::Alive, 2) | (Cell::Alive, 3) => Cell::Alive,
-                    // Rule 3: Any live cell with more than three live
-                    // neighbours dies, as if by overpopulation.
-                    (Cell::Alive, x) if x > 3 => Cell::Dead,
-                    // Rule 4: Any dead cell with exactly three live neighbours
-                    // becomes a live cell, as if by reproduction.
-                    (Cell::Dead, 3) => Cell::Alive,
-                    // All other cells remain in the same state.
-                    (otherwise, _) => otherwise,
+                    (Cell::Alive, _) => {
+                        self.make_dead(4 * idx);
+                        Cell::Dead
+                    }
+                    (Cell::Dead, 3) => {
+                        self.make_alive(4 * idx);
+                        Cell::Alive
+                    }
+                    _ => Cell::Dead,
                 };
-
-                next[idx] = next_cell;
+                *unsafe { self.cells_tmp.get_unchecked_mut(idx) } = next_cell;
             }
         }
-        self.cells = next;
+        mem::swap(&mut self.cells, &mut self.cells_tmp);
     }
 
-    pub fn new() -> Universe {
-        let width = 64;
-        let height = 64;
-
-        let cells = (0..width * height)
+    pub fn new(width: u32, height: u32) -> Universe {
+        let size = width * height;
+        let cells = (0..size)
             .map(|i| {
                 if i % 2 == 0 || i % 7 == 0 {
                     Cell::Alive
@@ -114,27 +139,15 @@ impl Universe {
             })
             .collect();
 
-        Universe {
+        let mut universe = Universe {
             width,
             height,
+            canvas: vec![0; 4 * size as usize],
             cells,
-        }
-    }
+            cells_tmp: vec![Cell::Dead; size as usize],
+        };
 
-    pub fn render(&self) -> String {
-        self.to_string()
-    }
-}
-
-impl fmt::Display for Universe {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        for line in self.cells.as_slice().chunks(self.width as usize) {
-            for &cell in line {
-                let symbol = if cell == Cell::Dead { '◻' } else { '◼' };
-                write!(f, "{}", symbol)?;
-            }
-            write!(f, "\n")?;
-        }
-        Ok(())
+        universe.update_canvas_from_cells();
+        universe
     }
 }
