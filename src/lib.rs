9f6c639f581417457603286652b7d16f143a2d27
Matthieu Pizenberg <matthieu.pizenberg@gmail.com>
2019-04-09 15:28:02 +0200

    Remove unnecessary stuff

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,5 @@
-mod utils;
-
-use cfg_if::cfg_if;
 use std::mem;
 use wasm_bindgen::prelude::*;
-
-cfg_if::cfg_if! {
-    // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
-    // allocator.
-    if #[cfg(feature = "wee_alloc")] {
-        #[global_allocator]
-        static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-    }
-}
 
 #[derive(Clone, Copy)]
 enum Cell {
